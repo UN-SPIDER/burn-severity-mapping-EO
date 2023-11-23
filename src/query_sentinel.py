e0d9100a2ec16bb3949ab6b247da221427abb5e3
Nick Gondek <npg@uchicago.edu>
2023-11-23 16:58:40 +0000

    SentinelHub costs money????? feeling deceived on this day of gratitude

---
+++ src/query_sentinel.py
@@ -0,0 +1,31 @@
+import requests
+import geopandas as gpd
+from pystac_client import Client
+from datetime import datetime
+
+SENTINEL2_PATH = "https://earth-search.aws.element84.com/v0"
+
+class Sentinel2Client:
+
+    def __init__(self, geojson_bounds):
+        self.path = SENTINEL2_PATH
+        self.client = Client.open(self.path)
+        self.geojson_bounds = geojson_bounds
+
+    def query(self, geojson_bounds, date_range, cloud_cover=20):
+        date_range_fmt = {
+            "gte": date_range[0],
+            "lte": date_range[1]
+        }
+        query = {
+            "collections": ["sentinel-s2-l2a-cogs"],
+            "intersects": geojson_bounds,
+            "datetime": date_range_fmt,
+            "query": {
+                "eo:cloud_cover": {
+                    "lt": cloud_cover
+                }
+            }
+        }
+        items = self.client.search(query).get_all_items()
+        return items
