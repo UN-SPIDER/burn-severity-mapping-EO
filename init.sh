077495836b9dfc79dcde4444fcee1c0f37455890
Nick Gondek <npg@uchicago.edu>
2023-11-28 03:56:37 +0000

    echo some debug, idk

--- init.sh
+++ init.sh
@@ -5,12 +5,14 @@ mamba env create -f environment.yml
 
 # Create the data directory if it doesn't exist
 mkdir -p workspace/data
+echo "Created directory"
 
 # Download the data
 curl -L -o workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+echo "Downloaded the data"
 
 # Unzip the data
 unzip workspace/data/baer_classifications.zip -d workspace/data
-
-# Activate the conda environment
-source activate burn-severity
+echo "Unzipped the data"
+echo "Data in workspace/data:"
+ll workspace/data
