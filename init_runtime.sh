a7af12adb057a762d89db38cae34414782a972a2
Nick Gondek <npg@uchicago.edu>
2023-11-28 04:16:59 +0000

    finally working, still fast AF at runtime so no worries there

--- init_runtime.sh
+++ init_runtime.sh
@@ -1,8 +1,8 @@
-# Create the data directory if it doesn't exist
-mkdir -p workspace/data
+# Create the data directory
+mkdir -p data
 
 # Download the data
-curl -L -o workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+curl -L -o data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
 
 # Unzip the data
-unzip workspace/data/baer_classifications.zip -d workspace/data
+unzip data/baer_classifications.zip -d data
