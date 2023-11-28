e41af2a102c25ae13dfde8ba2f39f648c1db1154
Nick Gondek <npg@uchicago.edu>
2023-11-28 18:03:18 +0000

    Didn't push this init.sh change from stale gp container... need to get better about managing these (tho should be easier once prebuild is worked out since I wont be validating/creating so many

--- init_prebuild.sh
+++ init_prebuild.sh
@@ -2,17 +2,3 @@
 
 # Create a new conda environment from the environment.yml file
 mamba env create -f environment.yml
-
-# Create the data directory if it doesn't exist
-mkdir -p workspace/data
-echo "Created directory"
-
-# Download the data
-curl -L -o workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
-echo "Downloaded the data"
-
-# Unzip the data
-unzip workspace/data/baer_classifications.zip -d workspace/data
-echo "Unzipped the data"
-echo "Data in workspace/data:"
-ls workspace/data
