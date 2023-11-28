3c683fd87771ced677e8df4735872dc4ab2f87ef
nick gondek <GondekNP@gmail.com>
2023-11-27 19:24:04 -0800

    don't cd to workspace - apparently that is not where the env.yaml lives, which is a bit confusing but maybe we just assume the contents of /workspace merge with the repo contents after build...?

--- init.sh
+++ init.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Change to the /workspace directory to persist our BAER data
-cd /workspace
-
 # Create a new conda environment from the environment.yml file
 mamba env create -f environment.yml
 
 # Create the data directory if it doesn't exist
-mkdir -p data
+mkdir -p /workspace/data
 
 # Download the data
-curl -L -o data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+curl -L -o /workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
 
 # Unzip the data
-unzip data/baer_classifications.zip -d data
+unzip /workspace/data/baer_classifications.zip -d /workspace/data
 
 # Activate the conda environment
 source activate burn-severity
