b5866087d6e885fedc1ca1d92cbe03489f9e036c
Nick Gondek <npg@uchicago.edu>
2023-11-28 03:41:52 +0000

    remove / from workspace - perhaps we are not running in root of container

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@
 mamba env create -f environment.yml
 
 # Create the data directory if it doesn't exist
-mkdir -p /workspace/data
+mkdir -p workspace/data
 
 # Download the data
-curl -L -o /workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+curl -L -o workspace/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
 
 # Unzip the data
-unzip /workspace/data/baer_classifications.zip -d /workspace/data
+unzip workspace/data/baer_classifications.zip -d workspace/data
 
 # Activate the conda environment
 source activate burn-severity
