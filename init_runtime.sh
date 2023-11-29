b6da21a44f53295dd7b62ee34b2652f39c715fc4
Nick Gondek <npg@uchicago.edu>
2023-11-29 04:17:01 +0000

    check if data exists before downloading, and properly conda init/source bashrc before attempting to activate our conda env

--- init_runtime.sh
+++ init_runtime.sh
@@ -1,11 +1,21 @@
-# Create the data directory
-mkdir -p data
+# Check if the data/baer directory exists
+if [ ! -d "/data/baer" ]
+then
+    # Create the data directory
+    mkdir -p data
 
-# Download the data
-curl -L -o data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+    # Download the data
+    curl -L -o data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
 
-# Unzip the data
-unzip data/baer_classifications.zip -d data
+    # Unzip the data
+    unzip data/baer_classifications.zip -d data
+fi
 
-# Activate the conda environment
+# Init the conda environment
+conda init bash
+
+# Need to source bashrc to get the init changes in the current shell
+source ~/.bashrc
+
+# Activate our environment
 conda activate burn-severity
