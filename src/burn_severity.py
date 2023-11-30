7ef3da843f0be84bbea6c2c5d3163135b2df6da4
Nick Gondek <npg@uchicago.edu>
2023-11-30 22:12:22 +0000

    predictable errors after big refactor into src logic from working notebook workflow

--- src/burn_severity.py
+++ src/burn_severity.py
@@ -17,6 +17,8 @@ from rasterio.mask import mask
 from shapely.geometry import mapping
 import geopandas as gpd
 import math
+import rioxarray as rxr
+import pandas as pd
 
 os.environ["AWS_NO_SIGN_REQUEST"] = "YES" # to avoid signing requests, avoid AWS auth
 
@@ -45,26 +47,74 @@ def read_band_image_from_stac_item_collection(band, item_collection):
     targetprj = osr.SpatialReference(wkt = images[0].crs.wkt)
     return mosaic, spatialRef, geoTransform, targetprj
 
-def nbr(band1, band2):
+def calc_nbr(band_nir, band_swir):
     """
     This function takes an input the arrays of the bands from the read_band_image
     function and returns the Normalized Burn ratio (NBR)
-    input:  band1   array (n x m)      array of first band image e.g B8A
-            band2   array (n x m)      array of second band image e.g. B12
+    input:  band_nir   array (n x m)      array of first band image e.g B8A
+            band_swir   array (n x m)      array of second band image e.g. B12
     output: nbr     array (n x m)      normalized burn ratio
     """
-    nbr = (band1 - band2) / (band1 + band2)
+    nbr = (band_nir - band_swir) / (band_nir + band_swir)
     return nbr
 
-def dnbr(nbr1,nbr2):
+def calc_dnbr(nbr_prefire, nbr_postfire):
     """
     This function takes as input the pre- and post-fire NBR and returns the dNBR
-    input:  nbr1     array (n x m)       pre-fire NBR
-            nbr2     array (n x m)       post-fire NBR
+    input:  nbr_prefire     array (n x m)       pre-fire NBR
+            nbr_postfire     array (n x m)       post-fire NBR
     output: dnbr     array (n x m)       dNBR
     """
-    dnbr = nbr1 - nbr2
+    dnbr = nbr_prefire - nbr_postfire
     return dnbr
+
+def calc_rdnbr(dnbr, nbr_prefire):
+    """
+    This function takes as input the dNBR and prefire NBR, and returns the relative dNBR
+    input:  dnbr     array (n x m)       dNBR
+            nbr_prefire     array (n x m)       pre-fire NBR
+    output: rdnbr    array (n x m)       relative dNBR
+    """
+    rdnbr = dnbr / np.abs(np.square(nbr_prefire))
+    return rdnbr
+
+def calc_rbr(dnbr, nbr_prefire):
+    """
+    This function takes as input the dNBR and prefire NBR, and returns the relative burn ratio
+    input:  dnbr     array (n x m)       dNBR
+            nbr_prefire     array (n x m)       pre-fire NBR
+    output: rbr    array (n x m)       relative burn ratio
+    """
+    rbr = dnbr / (nbr_prefire + 1.001)
+    return rbr
+
+def calc_burn_metrics(prefire_nir, prefire_swir, postfire_nir, postfire_swir):
+    """
+    This function takes as input the pre- and post-fire NIR and SWIR bands and returns the
+    NBR, dNBR, rDNBR, and rBR
+    input:  prefire_nir     array (n x m)       pre-fire NIR
+            prefire_swir     array (n x m)       pre-fire SWIR
+            postfire_nir     array (n x m)       post-fire NIR
+            postfire_swir     array (n x m)       post-fire SWIR
+    output: nbr_prefire     array (n x m)       normalized burn ratio
+            nbr_postfire     array (n x m)       normalized burn ratio
+            dnbr     array (n x m)       dNBR
+            rdnbr    array (n x m)       relative dNBR
+            rbr    array (n x m)       relative burn ratio
+    """
+    nbr_prefire = calc_nbr(prefire_nir, prefire_swir)
+    nbr_postfire = calc_nbr(postfire_nir, postfire_swir)
+    dnbr = calc_dnbr(nbr_prefire, nbr_postfire)
+    rdnbr = calc_rdnbr(dnbr, nbr_prefire)
+    rbr = calc_rbr(dnbr, nbr_prefire)
+    # stack these arrays together, naming them by their source
+
+    burn_stack = rxr.concat(
+        [nbr_prefire, nbr_postfire, dnbr, rdnbr, rbr],
+        pd.Index(['nbr_prefire', 'nbr_postfire', 'dnbr', 'rdnbr', 'rbr'], name='burn_metric')
+    )
+
+    return burn_stack
 
 def reproject_shp_gdal(infile, outfile, targetprj):
     """
--- src/query_sentinel.py
+++ src/query_sentinel.py
@@ -3,33 +3,45 @@ import geopandas as gpd
 from pystac_client import Client
 from datetime import datetime
 import planetary_computer
+import rioxarray as rxr
+import stackstac
+from .burn_severity import calc_burn_metrics
 
 SENTINEL2_PATH = "https://planetarycomputer.microsoft.com/api/stac/v1"
 
 
 class Sentinel2Client:
-    def __init__(self, geojson_bounds, buffer = .1):
+    def __init__(self, geojson_bounds, buffer = .1, crs = 4326, band_nir = "B8A", band_swir = "B12"):
         self.path = SENTINEL2_PATH
         self.client = Client.open(
             self.path,
             modifier = planetary_computer.sign_inplace
         )
-        self.geojson_bounds = geojson_bounds
+
+        self.band_nir = band_nir
+        self.band_swir = band_swir
+        self.crs = crs
+
+        # Buffer the bounds to ensure we get all the data we need, plus a
+        # little extra for visualization outside burn area
+
+        self.geojson_bounds = geojson_bounds.buffer(buffer).to_crs(crs)
         geojson_bbox = geojson_bounds.bounds.to_numpy()[0]
         self.bbox = [
-            geojson_bbox[0].round(decimals=2) - buffer,
-            geojson_bbox[1].round(decimals=2) - buffer,
-            geojson_bbox[2].round(decimals=2) + buffer,
-            geojson_bbox[3].round(decimals=2) + buffer
+            geojson_bbox[0] - buffer,
+            geojson_bbox[1] - buffer,
+            geojson_bbox[2] + buffer,
+            geojson_bbox[3] + buffer
         ]
 
 
+    def get_items(self, date_range, cloud_cover = 100, from_bbox = True, max_items = None):
+        
+        date_range_fmt = "{}/{}".format(date_range[0], date_range[1])
 
-    def query(self, date_range, cloud_cover=20, from_bbox = False, max_items = None):
-
-        date_range_fmt = "{}/{}".format(
-            date_range[0], date_range[1]
-        )
+        # Note - we might want to mess around with cloud cover eventually, but since we are aiming for 
+        # expediance in extreme events, we probably will want to make those determinations ourselves - 
+        # for now lets' look at everything
 
         query = {
             "collections": ["sentinel-2-l2a"],
@@ -48,3 +60,53 @@ class Sentinel2Client:
         items = self.client.search(**query).item_collection()
 
         return items
+
+    def arrange_stack(self, items, resolution = 20):
+
+        # Get CRS from first item (this isn't inferred by stackstac, for some reason)
+        stac_endpoint_crs = items[0].properties["proj:epsg"]
+
+        # Filter to our relevant bands and stack (again forcing the above crs, from the endpoint itself)
+        stack = stackstac.stack(
+            items,
+            epsg=stac_endpoint_crs,
+            resolution=resolution,
+            assets=[self.band_nir, self.band_swir]
+        ).rio.write_crs(stac_endpoint_crs)
+    
+        # Reproject to our desired CRS
+        stack = stack.rio.to_crs(self.crs)
+
+        # Clip to our bounds
+        stack = stack.rio.clip(self.geojson_bounds)
+
+        # Reduce according to our best approximation
+        reduced_stack = self.reduce_time_range(reduced_stack)
+
+        return reduced_stack
+
+    def reduce_time_range(self, range_stack):
+
+        # This will probably get a bit more sophisticated, but for now, just take the median
+        # We will probably run into issues of cloud occlusion, and for really long fire events,
+        # we might want to look into time-series effects of greenup, drying, etc, in the adjacent
+        # non-burned areas so attempt to isolate fire effects vs exogenous seasonal stuff. Ultimately,
+        # we just want a decent reducer to squash the time dim, so median works for now. 
+
+        return range_stack.median(dim = "time")
+         
+    def query_fire_event(self, prefire_date_range, postfire_date_range, from_bbox = True, max_items = None):
+
+        # Get items for pre and post fire range
+        prefire_items = self.get_items(prefire_date_range, from_bbox = from_bbox, max_items = max_items)
+        postfire_items = self.get_items(postfire_date_range, from_bbox = from_bbox, max_items = max_items)
+
+        self.prefire_stack = self.arrange_stack(prefire_items)
+        self.postfire_stack = self.arrange_stack(postfire_items)
+
+        self.metrics_stack = calc_burn_metrics(
+            prefire_nir = self.prefire_stack.sel(band = self.band_nir),
+            prefire_swir = self.prefire_stack.sel(band = self.band_swir),
+            postfire_nir = self.postfire_stack.sel(band = self.band_nir),
+            postfire_swir = self.postfire_stack.sel(band = self.band_swir),
+        )
