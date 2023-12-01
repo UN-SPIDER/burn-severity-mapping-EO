cba2d1c30e889fef5874e4c6e3f2ab115f8369cd
nick gondek <GondekNP@gmail.com>
2023-12-01 11:24:12 -0800

    chmod to make .sh executible

--- .devcontainer/init_runtime.sh
+++ .devcontainer/init_runtime.sh
@@ -1,20 +1,14 @@
-# Init conda
-source /opt/conda/etc/profile.d/conda.sh
-
 echo "Hello from runtime!"
-
-# Activate your conda environment
-conda activate burn-severity
 
 # Check if the data/baer directory exists, from a previous initialization of this container
 if [ ! -d "/workspace/burn-severity-mapping-poc/data" ]
 then
     # Create the data directory
-    mkdir -p data
+    mkdir -p /workspace/burn-severity-mapping-poc/data
 
     # Download the data
-    curl -L -o data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
+    curl -L -o /workspace/burn-severity-mapping-poc/data/baer_classifications.zip https://storage.googleapis.com/national_park_service/joshua_tree/BAER_Classifications/Fire_data_bundles_tiAWDyiXROUl0fjFRr8r.zip
 
     # Unzip the data
-    unzip data/baer_classifications.zip -d data
+    unzip /workspace/burn-severity-mapping-poc/data/baer_classifications.zip -d data
 fi
